d0e25048abf38138bd0a0c963f832cbc4083cbe7
YamaTatsu10969 <52.wadachi.96@gmail.com>
2019-11-13 18:34:01 +0900

    confirm build

---
+++ RealmTodoPlayground/TodoItem.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItem.swift
+//  RealmTodoPlayground
+//
+//  Created by yamamototatsuya on 2019/11/13.
+//  Copyright © 2019 yamamototatsuya. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class TodoItem: Object {
+    
+}
