bb769b81d689d7d289f1a49c3ae400f0b46079c5
YamaTatsu10969 <52.wadachi.96@gmail.com>
2019-11-14 16:32:58 +0900

    add comments _ delete unnecessary method

--- RealmTodoPlayground/TodoListViewController.swift
+++ RealmTodoPlayground/TodoListViewController.swift
@@ -27,6 +27,7 @@ class TodoListViewController: UIViewController {
         setupRealm()
         setupTableView()
         setupNavigationBar()
+        /// N をキーボードから押したら ダイアログが出る！！
         addKeyCommand(UIKeyCommand(title: "test", action: #selector(addButtonTapped(_:)), input: "N"))
     }
 }
@@ -62,10 +63,7 @@ private extension TodoListViewController {
         }))
         present(dialog, animated: true)
     }
-    
-    @objc func addButtonTapped2(_ sender: Any) {
-        print("どうも")
-    }
+
 }
 
 private extension TodoListViewController {
